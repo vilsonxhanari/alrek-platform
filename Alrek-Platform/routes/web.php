a9d8e267c80bc6b543467e56d3fc5a8fdc1354be
vilsonxhanari <vilsonxhani@gmail.com>
2024-09-17 16:01:04 -0500

    ok

--- Alrek-Platform/routes/web.php
+++ Alrek-Platform/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class,'index'])->name('home');
+Route::get('/shop/{id}', [ProductController::class,'show'])->name('shop');
+Route::get('/cart', [CartController::class,'cart'])->name('cart');
