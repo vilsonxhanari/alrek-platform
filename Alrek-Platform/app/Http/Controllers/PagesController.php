18e2ace4996619666140615a28b2c6fda37fc07f
vilsonxhanari <vilsonxhani@gmail.com>
2024-09-18 03:40:23 -0500

    homepage

--- Alrek-Platform/app/Http/Controllers/PagesController.php
+++ Alrek-Platform/app/Http/Controllers/PagesController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    /**
+     * Show the home page.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('pages.home'); // Make sure this view file exists
+    }
+
+    // Add other methods if needed
 }
